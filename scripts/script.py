d84839a2a1cb0f420940ce7fb4aa87b548d7ed79
dcheong <douglas.cheong1000@gmail.com>
2017-10-18 21:56:15 -0400

    snapshots

--- scripts/script.py
+++ scripts/script.py
@@ -1,14 +1,17 @@
 import os
 import sys
 import datetime
-import keyboard
 import io
 import picamera
 
+logDir = "/home/pi/SAUCGTC/"
 baseVidDir = "/home/pi/SAUCGTC/videos/"
 initTime = datetime.datetime.now()
 vidDir = baseVidDir + initTime.date().__str__() + "/"
 
+log = open(logDir + 'log', 'a+')
+log.write('Program started at ' + initTime.__str__() + '\n')
+log.close()
 # Video segments are 30s long
 videoLength = 30
 
@@ -16,27 +19,24 @@ videoLength = 30
 if not os.path.exists(vidDir):
   os.makedirs(vidDir)
 
-stream = io.BytesIO()
+#stream = io.BytesIO()
 camera = picamera.PiCamera()
-
 camera.start_preview(fullscreen=False, window = (100, 20, 640, 480))
 camera.resolution = (640,480)
 camera.start_recording(vidDir + initTime.time().__str__() + ".h264")
 
 while True:
-  if keyboard.is_pressed('q'):
-    camera.stop_recording()
-    camera.stop_preview()
-    camera.close()
-    print 'Quitting'
-    break
   timeNow = datetime.datetime.now()
+  print (timeNow - initTime).total_seconds()
   if timeNow.date().__str__() != initTime.date().__str__():
     if not os.path.exists(vidDir):
       os.makedirs(vidDir)
     vidDir = baseVidDir + timeNow.date().__str__() + "/"
   # Check if it is time to begin a new video segment
   if (timeNow - initTime).total_seconds() >= videoLength:
+    log = open(logDir + 'log', 'a')
+    log.write('Starting new recording at ' + vidDir + '\n')
+    log.close()
     print 'Starting new recording at ' + vidDir
     camera.stop_recording()
     initTime = timeNow
